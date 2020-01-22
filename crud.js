9a8a823bf6ab41da2c14941ff9a58382e1da8f55
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-22 11:31:44 +0530

    crud app - Switch Case Add , Add, delete , update functionality

---
+++ crud.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const userOperations = require('./crudOperations');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let body = '';
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/JSON');
+  switch (req.url, req.method) {
+    case ('/add', 'POST'):
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.addUser(body));
+      });
+      break;
+
+
+    case ('/delete', 'DELETE'):
+      req.on('data', function (data) {        
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.deleteUser(JSON.parse(body)));
+      });
+      break;
+
+
+    case ('/edit', 'PUT'):
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.updateUser(JSON.parse(body)));
+      });
+      break;
+    case ('/read', 'GET'):
+      req.on('data', function (data) {
+        body = body + data;
+      });
+      req.on('end', function () {
+        res.end(userOperations.getUser(JSON.parse(body)));
+      });
+      break;
+    default:
+      break;
+  }
+}).listen(port, () => {
+  console.log(`Server running at ${port}/`);
+});
---
+++ crudOperations.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const db = 'data.json';
+let flag = 0, userRecord = '';
+const user_records = JSON.parse(fs.readFileSync(db));
+const userArray = user_records.map((items) => { return items; });
+
+function commonfun(statusCode, status, message, data = "") {
+	let stats = {
+		statusCode,
+		status,
+		message,
+		data
+	}
+	return JSON.stringify(stats);
+}
+
+function addUser(data) {
+	const userData = JSON.parse(data);
+	let msg;
+	user_records.forEach(items => {
+		flag = 0;
+		if (items.id === userData.id) {
+			msg = 'id is already exist..!';
+		} else if (items.name == userData.name) {
+			msg = 'email is already exist..!';
+		} else if (items.email == userData.email) {
+			msg = 'contact is already exist..!';
+		}
+
+		if (items.id != userData.id) {
+			if (items.name != userData.name) {
+				if (items.email != userData.email) {
+					flag = 1;
+				}
+			}
+		}
+	})
+
+	if (flag = 1) {
+		userArray.push(userData);
+		fs.writeFile(db, JSON.stringify(userArray), function (err) {
+			if (err) return commonfun("502", "Error", err);
+		});
+		return commonfun(200, 'Ok', 'User added Successfully');
+	}
+	else {
+		return commonfun(200, 'Ok', msg);
+	}
+}
+
+function deleteUser(data) {
+	const deleteRecord = user_records.splice(user_records.findIndex(() => {
+		return data.id;
+	}), 1);
+	if (deleteRecord) {		
+		fs.writeFileSync(db, JSON.stringify(user_records));
+		return commonfun("200", "OK", 'User Deleted Successfully..!');
+	}
+	else {
+		return commonfun("200", "OK", "No User Found..!");
+	}
+}
+
+
+module.exports = {
+	addUser,
+	deleteUser,
+	commonfun
+}
